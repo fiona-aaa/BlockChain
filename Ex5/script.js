8e9968f39f76e330d9936b4ad4279d39b7032d97
fiona-aaa <2356408976@qq.com>
2022-12-22 20:51:01 +0800

    报告

--- Ex5/script.js
+++ Ex5/script.js
@@ -16,7 +16,7 @@ console.log("web3:"+web3); //然后再输出web3
 if (typeof web3 !== 'undefined')  {
 	web3 = new Web3(web3.currentProvider);
 } else {
-	web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+	web3 = new Web3(new web3.providers.HttpProvider("http://localhost:8545"));
 }
 
 // Default account is the first one
